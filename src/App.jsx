057902c526f5bc19c38ac9fd38d0b7a3a47078bc
james lyddon <jameslyddon@gmail.com>
2025-08-05 16:53:03 +0100

    did some tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { bands } from './data/bandsData'
+import { gigs } from './data/bandsData'
 import Gig from "./components/gig/Gig"
 import "./App.css";
-
-
 
 function App() {
   return (
     <div className="gigsContainer">
-      <Gig band={bands.bearsuit} />
-      <Gig band={bands.theSonicManipulator} />
-      <Gig band={bands.TWRP} />
-      <Gig band={bands.gojira} />
+      {gigs.map(gig => (
+        <Gig band={gig} key={gig.id} />
+      ))}
     </div>
   );
 }
--- src/data/bandsData.js
+++ src/data/bandsData.js
@@ -1,30 +1,34 @@
-export const bands = {
-    bearsuit: {
+export const gigs = [
+  {
+    id: '810fba5c-cbcc-451c-ba78-c716acbfd244',
     name: "Bearsuit",
     img: "https://live.staticflickr.com/3182/2971845408_149d60194c_z.jpg",
     description: "Bearsuit are an English art-rock/indie pop band from Norwich, England, active from 2001",
     time: "8pm, 20th of September, 2025",
     location: "The Water Rats, 328 Grays Inn Road, Kings Cross, WC1X 8BZ"
   }, 
-    theSonicManipulator: {
+  {
+    id: 'e55d5319-8077-473c-8d8f-46cb73cc6d9a',
     name: "The Sonic Manipulator",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ4tXrA0dbrwuYng17JB0d7dTjdt8mYJSoNA&s",
     description: "Greetings Earthlings, I have had many interesting adventures on my way to becoming a spaceman, such is the nature of the convolutions of life.",
     time: "5pm, 11th of November, 2025",
     location: "Sydney Opera House, Bennelong Point, Sydney NSW 2000, Australia"
   }, 
-    TWRP: {
+  {
+    id: '97078b0b-5c1c-405c-8f8b-f1e04b06e013',
     name: "TWRP",
     img: "https://acomicsoul.com/wp-content/uploads/2025/01/434212848_402209732454525_425956686006349285_n.jpg",
     description: "TWRP, formerly known as Tupper Ware Remix Party, is a Canadian rock band originally from Halifax, Nova Scotia. Founded in 2007, the band moved to Toronto in 2011 with a change in members and musical direction.",
     time: "5pm, 11th of November, 2025",
     location: "Massey Hall, Toronto, Canada"
   }, 
-    gojira: {
+  {
+    id: '4632f1a4-1dc5-41b3-857a-88cfc51474c5',
     name: "Gojira",
     img: "https://metaloda.com/storage/2021/12/gojira3.jpg",
     description: "Gojira is a French heavy metal band from Ondres. Founded as Godzilla in 1996, the band's lineup - brothers Joe and Mario Duplantier, Christian Andreu, and Jean-Michel Labadie - has been the same since the band changed its name to Gojira in 2001.",
     time: "9pm, 15th of November, 2025",
     location: "Salle Capranie, Ondres, France"
   }, 
-}
+]
