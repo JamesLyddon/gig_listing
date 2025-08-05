7bdd2e949247430a4f70509bec72eef82aa6f27f
james lyddon <jameslyddon@gmail.com>
2025-08-05 11:10:03 +0100

    about to mess up css

--- src/components/gig/Gig.jsx
+++ src/components/gig/Gig.jsx
@@ -4,15 +4,17 @@ const Gig = ({band}) => {
     const {name, description, location, time, img} = band
     // set image as background for card
     const cardStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9)), url(${img})`
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(${img})`
     };
 
     return (
         <div className="card" style={cardStyle}>
-            <h3 className="bandName">{name}</h3>
-            <p className="bandDescription" >{description}</p>
-            <p className="gigLocation">{location}</p>
-            <p className="gigTime">{time}</p>
+
+                <h3 className="bandName">{name}</h3>
+                <p className="bandDescription" >{description}</p>
+                <p className="gigTime">{time}</p>
+                <p className="gigLocation">{location}</p>
+
         </div>
     )
 }
